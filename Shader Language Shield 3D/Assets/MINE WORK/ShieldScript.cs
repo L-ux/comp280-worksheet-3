85bc267a9d5a2934085d1c053965b864f65628f2
JOSHUA GRILLO <JG215923@falmouth.ac.uk>
2019-12-10 10:07:22 +0000

    Moved to a non-LWRP project because of import errors and the such

---
+++ Shader Language Shield 3D/Assets/MINE WORK/ShieldScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShieldScript : MonoBehaviour
+{
+    public int repeatLimit;
+    public int speed;
+
+    public Material ShieldMaterial;
+
+    private Vector4 wavePoke = Vector4.one;
+    public Vector3 WavePoke { set { wavePoke = new Vector4(value.x, value.y, value.z, 0); } }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("SS Update");
+        wavePoke.w  = Mathf.Min (wavePoke.w + (Time.deltaTime * speed) ,1);
+        ShieldMaterial.SetVector("_WaveOrigin", wavePoke);
+    }
+}
